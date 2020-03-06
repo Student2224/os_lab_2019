cf20a0193588377bbd73eb2a40f1b60a1de2d29c
student2224 <as16946032@gmail.com>
2020-03-06 10:39:39 +0000

    zad2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,15 @@
 
 void RevertString(char *str)
 {
+    char *buff=malloc(sizeof(char)*(strlen(str)+1));
+    int i=0;
+
+    strcpy(buff, str);
+    for(i=0;i<strlen(str);i++)
+    {       
+        str[strlen(str)-1-i]=buff[i];
+    }
+   // *str=*buff;
 	// your code here
 }
 
